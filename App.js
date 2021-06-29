4be1cc38422eae84cb3029cd895fa4b6065510cd
Maya Imhoff <mayelyimhoff@gmail.com>
2021-06-29 12:45:23 +0200

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  ScrollView,
+  TextInput,
+  Image,
+} from "react-native";
+import Example from "./src/components/Example";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Example />
+      <View style={styles.container}>
+        <Text style={styles.maya}>Hello, Okan what up yo!</Text>
+        <Text>Hello, Okan what up yo!</Text>
+        <Text>Hello, Okan what up yo!</Text>
+        <Image
+          source={{
+            uri: "https://reactnative.dev/docs/assets/p_cat2.png",
+          }}
+          style={{ width: 200, height: 200 }}
+        />
+        <Example />
+        <TextInput
+          style={{
+            height: 40,
+            borderColor: "gray",
+            borderWidth: 1,
+            padding: 5,
+            margin: 15,
+          }}
+          defaultValue="You can type in me"
+        />
+      </View>
+      <ScrollView style={{ backgroundColor: "#d709f7" }}>
+        <Text style={{ fontSize: 96 }}>Scroll me plz</Text>
+
+        <Image
+          source={{
+            uri: "https://reactnative.dev/docs/assets/p_cat2.png",
+          }}
+          style={{ width: 200, height: 200 }}
+        />
+
+        <Text style={{ fontSize: 96 }}>Scrolling down</Text>
+        <Image
+          source={{
+            uri: "https://reactnative.dev/docs/assets/p_cat2.png",
+          }}
+          style={{ width: 200, height: 200 }}
+        />
+
+        <Text style={{ fontSize: 96 }}>Framework around?</Text>
+
+        <Text style={{ fontSize: 80 }}>React Native</Text>
+      </ScrollView>
+    </>
   );
 }
 
+// const styles = {
+//   container: {
+//     // flex: 1,
+//     backgroundColor: "#d709f7",
+//     alignItems: "center",
+//     justifyContent: "center",
+//     paddingTop: 80,
+//   },
+//   maya: {
+//     color: "green",
+//   },
+// };
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // flex: 1,
+    backgroundColor: "#d709f7",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 80,
+  },
+  maya: {
+    color: "green",
   },
 });
---
+++ src/components/Example.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+  Text,
+  Button,
+  InputAccessoryView,
+  ScrollView,
+  TextInput,
+} from "react-native";
+
+const Example = () => {
+  return <Text>Hello, I am an example component</Text>;
+};
+
+export default Example;
